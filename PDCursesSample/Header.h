2741f1d52a438b0aa6e7992f5cdc254767db33b8
Naoyuki-Kita <cgud1025@mail4.doshisha.ac.jp>
2021-07-07 09:43:49 +0900

    #1, #2, #4, #5 の解決

--- PDCursesSample/Header.h
+++ PDCursesSample/Header.h
@@ -1,41 +1,40 @@
 #pragma once
 
 #define BUFFSIZE 128
-#define MAX_WIDTH 50
 #define MAX_HEIGHT 50
+#define MAX_WIDTH 50
 
-
-struct Coordinate {
-	int x;
-	int y;
-};
-
-struct Digger {
-	Coordinate* current_coordinate;			//åªç›ÇÃç¿ïW
-	Coordinate* previous_coordinate;		//ëOÇÃç¿ïW
-};
 
 struct Maze {
-	int width;
 	int height;
-	int data[MAX_WIDTH][MAX_HEIGHT];
+	int width;
+	int player_x;
+	int player_y;
+	int goal_x;
+	int goal_y;
+	int data[MAX_HEIGHT][MAX_WIDTH];
 };
 
-Maze init_maze(int width, int height);
+void getCurrentDirectory(char* currentDirectory);
 
-Coordinate init_coordinate(int x, int y);
+Maze init_maze(int height, int width);
 
 void set_num(Maze* maze, int x, int y, int num);
 
 int get_num(Maze* maze, int x, int y);
 
-int dig(Maze* maze, Coordinate coordinate);
+int dig(Maze* maze, int x, int y);
 
 void dig_maze(Maze* maze, int start_x, int start_y);
 
 void shuffle(int array[], int size, unsigned int seed);
 
-//void init_man(Maze maze);
+void init_man(Maze* maze);
+
+void set_goal(Maze* maze);
 
 void print_maze(Maze maze);
 
+void tprint_maze(Maze maze);
+
+void move_man(Maze* maze, int key);
--- PDCursesSample/PDCursesSample.cpp
+++ PDCursesSample/PDCursesSample.cpp
@@ -8,29 +8,110 @@
 #include <time.h>
 
 #define BUFFSIZE 128
-#define WALL 1
 #define PATH 0
-#define OUT 9
+#define WALL 1
 #define MAN 2
+#define GOAL 3
+#define OUT 9
 
 
 int main() {
 
-	const int width = 30;
+	/*char currentDirectory[BUFFSIZE];
+	getCurrentDirectory(currentDirectory);
+	fprintf_s(stdout, currentDirectory);
+	char section[BUFFSIZE];
+	sprintf_s(section, "section1");
+	char height_label[BUFFSIZE];
+	sprintf_s(height_label, "height");
+	char width_label[BUFFSIZE];
+	sprintf_s(width_label, "label");
+	char settingFile[BUFFSIZE];
+	sprintf_s(settingFile, "%s¥¥setting.ini", currentDirectory);
+    int height = GetPrivateProfileInt(section, height_label, -1, settingFile);
+	int width = GetPrivateProfileInt(section, width_label, -1, settingFile);*/
+
 	const int height = 30;
+	const int width = 30;
+
+	int key = 0;
 
 	initscr();
+	noecho();
+	cbreak();
 
-	Maze maze = init_maze(width, height);
-	dig_maze(&maze, 2, 3);
-	//init_man(maze);
-
-    //print_maze(maze);
-
-	print_maze(maze);
-
+	/*start state=================================================*/
+	mvaddstr(10, 10, "MAZE by K.N.");
+	mvaddstr(11, 10, "PRESS ANY KEY");
+	refresh();
 	getch();
+	/*============================================================*/
+
+
+	/*creating maze===============================================*/
+	srand((unsigned int)clock());
+
+	int start_x = 2 * (rand() % ((height / 2) - 1)) + 1;
+	int start_y = 2 * (rand() % ((width / 2) - 1)) + 1;
+
+	Maze maze = init_maze(height, width);
+	dig_maze(&maze, start_x, start_y);
+	init_man(&maze);
+	set_goal(&maze);
+	/*============================================================*/
+	
+
+	/*playing state===============================================*/
+	keypad(stdscr, TRUE);
+	while (true) {
+		key = getch();
+		move_man(&maze, key);
+		if (maze.player_x == maze.goal_x && maze.player_y == maze.goal_y) break;
+	}
+	/*============================================================*/
+
+
+	/*end state===================================================*/
 	endwin();
+	fprintf_s(stdout, "maze status:\n");
+	fprintf_s(stdout, "\tgoal_x\t%d\n", maze.goal_x);
+	fprintf_s(stdout, "\tgoal_y\t%d\n", maze.goal_y);
+
+	/*FILE* fp;
+	errno_t error = fopen_s(&fp, "result.txt", "w");
+	if (error != 0) {
+		fprintf_s(stderr, "failed to open");
+	}
+	else
+	{
+		fprintf_s(stdout, "success to open");
+		for (int x = 0; x < maze.height; x++) {
+			for (int y = 0; y < maze.width; y++) {
+				switch (get_num(&maze, x, y)) {
+				case PATH:
+					fputs("　", fp);
+					break;
+				case WALL:
+					fputs("■", fp);
+					break;
+				case MAN:
+					fputs("人", fp);
+					break;
+				case GOAL:
+					fputs("出", fp);
+					break;
+				case OUT:
+					fputs("？", fp);
+					break;
+				}
+			}
+			fputs("\n", fp);
+		}
+		fclose(fp);
+	}*/
+	/*============================================================*/
+
+	tprint_maze(maze);
 
 	return 0;
 }
