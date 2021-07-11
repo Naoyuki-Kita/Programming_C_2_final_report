f278804b5d2e8eab3478d20959f8e17c2f057efb
Naoyuki-Kita <cgud1025@mail4.doshisha.ac.jp>
2021-07-11 12:34:38 +0900

    #3 入出力ファイルの設定

--- PDCursesSample/PDCursesSample.cpp
+++ PDCursesSample/PDCursesSample.cpp
@@ -8,29 +8,123 @@
 #include <time.h>
 
 #define BUFFSIZE 128
-#define WALL 1
 #define PATH 0
-#define OUT 9
+#define WALL 1
 #define MAN 2
+#define GOAL 3
+#define OUT 9
 
 
 int main() {
 
-	const int width = 30;
-	const int height = 30;
+	int height = -1;
+	int width = -1;
+	int key = 0;
+	int move_ctr = 0;
+	char buff[BUFFSIZE];
+
+	char currentDirectory[BUFFSIZE];
+	getCurrentDirectory(currentDirectory);
+	char section[BUFFSIZE];
+	sprintf_s(section, "section1");
+	char height_label[BUFFSIZE];
+	sprintf_s(height_label, "height");
+	char width_label[BUFFSIZE];
+	sprintf_s(width_label, "width");
+	char settingFile[BUFFSIZE];
+	sprintf_s(settingFile, "%s\\settings.ini", currentDirectory);
+	height = GetPrivateProfileInt(section, height_label, -1, settingFile);
+	width = GetPrivateProfileInt(section, width_label, -1, settingFile);
 
 	initscr();
+	noecho();
+	cbreak();
 
-	Maze maze = init_maze(width, height);
-	dig_maze(&maze, 2, 3);
-	//init_man(maze);
 
-    //print_maze(maze);
-
-	print_maze(maze);
-
+	/*start state=================================================*/
+	mvaddstr(10, 10, "MAZE by K.N.");
+	mvaddstr(11, 10, "PRESS ANY KEY");
+	refresh();
 	getch();
-	endwin();
+	/*============================================================*/
+
+
+	/*creating maze===============================================*/
+	srand((unsigned int)clock());
+
+	int start_x = 2 * (rand() % ((height / 2) - 1)) + 1;
+	int start_y = 2 * (rand() % ((width / 2) - 1)) + 1;
+
+	Maze maze = init_maze(height, width);
+	dig_maze(&maze, start_x, start_y);
+	init_man(&maze);
+	set_goal(&maze);
+
+	FILE* fp;
+	errno_t error = fopen_s(&fp, "result.txt", "w");
+	if (error != 0) {
+		fprintf_s(stderr, "failed to open\n");
+	}
+	else
+	{
+		fprintf_s(stdout, "success to open\n");
+		for (int x = 0; x < maze.height; x++) {
+			for (int y = 0; y < maze.width; y++) {
+				switch (get_num(&maze, x, y)) {
+				case PATH:
+					fputs("　", fp);
+					break;
+				case WALL:
+					fputs("■", fp);
+					break;
+				case MAN:
+					fputs("人", fp);
+					break;
+				case GOAL:
+					fputs("出", fp);
+					break;
+				case OUT:
+					fputs("？", fp);
+					break;
+				}
+			}
+			fputs("\n", fp);
+		}
+	/*============================================================*/
+
+	/*playing state===============================================*/
+		keypad(stdscr, TRUE);
+		while (true) {
+			key = getch();
+			if (move_man(&maze, key) != 0) move_ctr++;
+			if (maze.player_x == maze.goal_x && maze.player_y == maze.goal_y) break;
+		}
+	/*============================================================*/
+
+	/*end state===================================================*/
+		endwin();
+	
+		sprintf_s(buff, "移動回数:  %d\n", move_ctr);
+		fputs(buff, fp);
+
+		fclose(fp);
+	/*============================================================*/
+	}
+	
+
+
+	
+	
+	
+
+
+	
+	
+
+	
+	
+
+	tprint_maze(maze);
 
 	return 0;
 }
--- PDCursesSample/functions.cpp
+++ PDCursesSample/functions.cpp
@@ -3,60 +3,63 @@
 #include <curses.h>
 #include <Windows.h>
 #include <time.h>
+#include "string.h"
 
 
 #define PATH 0
 #define WALL 1
 #define MAN 2
+#define GOAL 3
 #define OUT 9
+#define BUFFSIZE 128
 
-// 0が通路、1が壁、2が人、9が領域外
+// 0が通路、1が壁、2が人、3がゴール、9が領域外
 
 enum  Direction { UP, DOWN, RIGHT, LEFT };
 
 
-Maze init_maze(int width, int height) {
+void getCurrentDirectory(char* currentDirectory) {
+	GetCurrentDirectory(BUFFSIZE, currentDirectory);
+}
+
+Maze init_maze(int height, int width) {
 
 	Maze maze;
 
-	maze.width = width;
 	maze.height = height;
+	maze.width = width;
+	maze.player_x = -1;
+	maze.player_y = -1;
+	maze.goal_x = -2;
+	maze.goal_y = -2;
 
-	for (int x = 0; x < MAX_WIDTH; x++) {
-		for (int y = 0; y < MAX_HEIGHT; y++) {
+	for (int x = 0; x < MAX_HEIGHT; x++) {
+		for (int y = 0; y < MAX_WIDTH; y++) {
 			maze.data[x][y] = OUT;
 		}
 	}
-	
-	for (int x = 0; x < width; x++) {;
-		for (int y = 0; y < height; y++) {
+
+	for (int x = 0; x < height; x++) {
+		;
+		for (int y = 0; y < width; y++) {
 			set_num(&maze, x, y, WALL);
 		}
 	}
 	return maze;
 }
 
-Coordinate init_coordinate(int x, int y) {
-	Coordinate coordinate;
-	coordinate.x = x;
-	coordinate.y = y;
-	return coordinate;
-}
-
 
 void set_num(Maze* maze, int x, int y, int num) {
-	maze->data[x+1][y+1] = num;
+	maze->data[x + 1][y + 1] = num;
 }
 
 
 int get_num(Maze* maze, int x, int y) {
-	return maze->data[x+1][y+1];
+	return maze->data[x + 1][y + 1];
 }
 
-int dig(Maze* maze, Coordinate coordinate) {
+int dig(Maze* maze, int x, int y) {
 
-	int x = coordinate.x;
-	int y = coordinate.y;
 	int flag_ctr = 0;
 
 	if (get_num(maze, x, y) != WALL) return 0;
@@ -81,13 +84,8 @@ int dig(Maze* maze, Coordinate coordinate) {
 }
 
 void dig_maze(Maze* maze, int start_x, int start_y) {
-	//Sleep()で実行時間を変化させランダム性を持たせている状態
-	//表示まで10秒弱かかる
-	//dig_maze中にprintして変化の様子を可視化することで実行時間を稼ぎたい
-	
-	//erase();
-	//print_maze(*maze);
-	//init_rand();
+	refresh();
+	print_maze(*maze);
 
 	set_num(maze, start_x, start_y, PATH);
 	Sleep(1);
@@ -96,10 +94,19 @@ void dig_maze(Maze* maze, int start_x, int start_y) {
 	shuffle(dir_array, 4, (unsigned int)clock());
 
 	for (int i = 0; i < 4; i++) {
-		if (dir_array[i] == UP && dig(maze, init_coordinate(start_x, (start_y - 1))) == 1) dig_maze(maze, start_x, (start_y - 1));
-		if (dir_array[i] == DOWN && dig(maze, init_coordinate(start_x, (start_y + 1))) == 1) dig_maze(maze, start_x, (start_y + 1));
-		if (dir_array[i] == RIGHT && dig(maze, init_coordinate((start_x + 1), start_y)) == 1) dig_maze(maze, (start_x + 1), start_y);
-		if (dir_array[i] == LEFT && dig(maze, init_coordinate((start_x - 1), start_y)) == 1) dig_maze(maze, (start_x - 1), start_y);
+		erase();
+		if (dir_array[i] == UP && dig(maze, start_x, (start_y - 1)) == 1) {
+			dig_maze(maze, start_x, (start_y - 1));
+		}
+		if (dir_array[i] == DOWN && dig(maze, start_x, (start_y + 1)) == 1) {
+			dig_maze(maze, start_x, (start_y + 1));
+		}
+		if (dir_array[i] == RIGHT && dig(maze, (start_x + 1), start_y) == 1) {
+			dig_maze(maze, (start_x + 1), start_y);
+		}
+		if (dir_array[i] == LEFT && dig(maze, (start_x - 1), start_y) == 1) {
+			dig_maze(maze, (start_x - 1), start_y);
+		}
 	}
 }
 
@@ -113,39 +120,123 @@ void shuffle(int array[], int size, unsigned int seed) {
 	}
 }
 
-//void init_man(Maze maze) {
-//	init_rand();
-//
-//	int start_x = 0;
-//	int start_y = 0;
-//
-//	while (get_num(maze, start_x, start_y) != PATH) {
-//		start_x = 2 * (rand() % (maze.width / 2)) + 1;
-//		start_y = 2 * (rand() % (maze.height / 2)) + 1;
-//	}
-//
-//	set_num(maze, start_x, start_y, MAN);
-//}
+void init_man(Maze* maze) {
+
+	int x = -1;
+	int y = -1;
+
+	for (int sum = 0; sum < min(maze->height, maze->width); sum++) {
+		for (x = 0; x < sum; x++) {
+			y = sum - x;
+			if (get_num(maze, x, y) == PATH) goto break_state;
+		}
+	}
+
+break_state:
+	maze->player_x = x;
+	maze->player_y = y;
+	set_num(maze, maze->player_x, maze->player_y, MAN);
+
+	refresh();
+	print_maze(*maze);
+}
+
+void set_goal(Maze* maze) {
+	int x = -2;
+	int y = -2;
+
+	for (int sum = maze->height + maze->width; sum > 0; sum--) {
+		for (x = maze->height; x > 0; x--) {
+			y = sum - x;
+			if (y < maze->width && get_num(maze, x, y) == PATH) goto break_state;
+		}
+	}
+break_state:
+	maze->goal_x = x;
+	maze->goal_y = y;
+	set_num(maze, maze->goal_x, maze->goal_y, GOAL);
+
+	refresh();
+	print_maze(*maze);
+}
 
 void print_maze(Maze maze) {
 	erase();
-	for (int x = 0; x < maze.width; x++) {
-		for (int y = 0; y < maze.height; y++) {
+	for (int x = 0; x < maze.height; x++) {
+		for (int y = 0; y < maze.width; y++) {
 			switch (get_num(&maze, x, y)) {
 			case PATH:
-				mvaddstr(x, 2*y, "　");
+				mvaddstr(x, 2 * y, "　");
 				break;
 			case WALL:
-				mvaddstr(x, 2*y, "■");
+				mvaddstr(x, 2 * y, "■");
 				break;
 			case MAN:
-				mvaddstr(x, 2*y, "○");
+				mvaddstr(x, 2 * y, "人");
+				break;
+			case GOAL:
+				mvaddstr(x, 2 * y, "出");
 				break;
 			case OUT:
-				mvaddstr(x, 2*y, "？");
+				mvaddstr(x, 2 * y, "？");
 				break;
 			}
 		}
 	}
 	refresh();
+}
+
+void tprint_maze(Maze maze) {
+	for (int x = 0; x < maze.height; x++) {
+		for (int y = 0; y < maze.width; y++) {
+			switch (get_num(&maze, x, y)) {
+			case PATH:
+				fprintf_s(stdout, "　");
+				break;
+			case WALL:
+				fprintf_s(stdout, "■");
+				break;
+			case MAN:
+				fprintf_s(stdout, "人");
+				break;
+			case GOAL:
+				fprintf_s(stdout, "出");
+				break;
+			case OUT:
+				fprintf_s(stdout, "？");
+				break;
+			}
+		}
+		fprintf_s(stdout, "\n");
+	}
+}
+
+int move_man(Maze* maze, int key) {
+	int prev_x = maze->player_x;
+	int prev_y = maze->player_y;
+	int answer = 1;
+	switch (key) {
+	case KEY_UP:
+		maze->player_x--;
+		break;
+	case KEY_DOWN:
+		maze->player_x++;
+		break;
+	case KEY_RIGHT:
+		maze->player_y++;
+		break;
+	case KEY_LEFT:
+		maze->player_y--;
+		break;
+	}
+	if (get_num(maze, maze->player_x, maze->player_y) == WALL) {
+		maze->player_x = prev_x;
+		maze->player_y = prev_y;
+		answer = 0;
+	}
+	set_num(maze, prev_x, prev_y, PATH);
+	set_num(maze, maze->player_x, maze->player_y, MAN);
+	refresh();
+	print_maze(*maze);
+	return answer;
 }
